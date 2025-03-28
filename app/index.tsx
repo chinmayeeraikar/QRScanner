9a1dfee57e75686bd148cd7a2f5cbf09f45010ca
Chinmayee Raikar <chinmayeeraikar@gmail.com>
2025-03-28 16:54:36 +0530

    working?

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,24 @@
-import { Text, View, StyleSheet } from 'react-native';
- import { Link } from 'expo-router'; 
+import { Text, View, StyleSheet, SafeAreaView, Pressable } from 'react-native';
+ import { Link, Stack } from 'expo-router'; 
+import { useCameraPermissions } from 'expo-camera';
 
 export default function Index() {
+
+  const [ permission, requestPermission] = useCameraPermissions();
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
+    <SafeAreaView>
+      <Stack.Screen options = {{ title: "Overview", headerShown: false}}/>
+      <Text style={styles.text}>QR Code Scanner</Text>
+      {/* <Link href="/about" style={styles.button}>
         Go to About screen
-      </Link>
-    </View>
+      </Link> */}
+      <View style = {{ gap: 20 }}>
+        <Pressable onPress={requestPermission}>
+          <Text style = {styles.button}>Request Permissions</Text>
+        </Pressable>
+      </View>
+    </SafeAreaView>
   );
 }
 
